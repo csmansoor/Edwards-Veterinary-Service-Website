e50ae1a9e6aab7c5ec44ac81315a05bf50f446e3
csmansoor <157922526+csmansoor@users.noreply.github.com>
2026-01-21 16:14:33 +0500

    Fixed path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   
-  base: './',
+  base: '/Edwards-Veterinary-Service-Website/',
   server: {
     port: 5173,
     open: true
