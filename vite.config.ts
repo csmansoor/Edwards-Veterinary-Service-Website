addd4fcd74863035014cff3ae1bedc66bf1cd099
csmansoor <157922526+csmansoor@users.noreply.github.com>
2026-01-21 15:03:27 +0500

    Fixed  Github pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   
-  base: '/Vet-clinic-website/',
+  base: '/Edwards-Veterinary-Service-Website/',
   server: {
     port: 5173,
     open: true
